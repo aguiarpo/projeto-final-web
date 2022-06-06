209a8e1f50b6c69bc0b78b79ce519539a8a64d69
Eduardo Aguiar <eduardo.poerner@catolicasc.org.br>
2022-06-06 14:45:03 -0300

    Initial commit

---
+++ src/test/java/br/com/projeto/biblioteca/ProjetoFinalApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.projeto.biblioteca;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetoFinalApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
