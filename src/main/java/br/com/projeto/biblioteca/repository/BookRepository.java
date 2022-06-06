bb9c0e0bce85e2fb6e8a3fa54d3cacd1fffbbf05
Eduardo Aguiar <eduardo.poerner@catolicasc.org.br>
2022-06-06 16:51:12 -0300

    Adicionado finds

--- src/main/java/br/com/projeto/biblioteca/repository/BookRepository.java
+++ src/main/java/br/com/projeto/biblioteca/repository/BookRepository.java
@@ -1,10 +1,15 @@
 package br.com.projeto.biblioteca.repository;
 
 import br.com.projeto.biblioteca.models.Book;
+import br.com.projeto.biblioteca.models.User;
+import br.com.projeto.biblioteca.models.enums.Genre;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface BookRepository extends PagingAndSortingRepository<Book, Long> {
-
+    Page<User> findByName(String name, Pageable pageable);
+    Page<User> findByGenre(Genre genre, Pageable pageable);
 }
--- src/main/java/br/com/projeto/biblioteca/repository/UserRepository.java
+++ src/main/java/br/com/projeto/biblioteca/repository/UserRepository.java
@@ -1,9 +1,21 @@
 package br.com.projeto.biblioteca.repository;
 
-import br.com.projeto.biblioteca.models.Book;
+import br.com.projeto.biblioteca.models.Loan;
+import br.com.projeto.biblioteca.models.User;
+import br.com.projeto.biblioteca.models.enums.TypeUsers;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
-public interface UserRepository extends PagingAndSortingRepository<Book, Long> {
+public interface UserRepository extends PagingAndSortingRepository<User, Long> {
+    Page<User> findByEmail(String email, Pageable pageable);
+    Page<User> findByName(String name, Pageable pageable);
+    Optional<User> findByEmailAndPassword(String email, String password);
+
+    Page<User> findByType(TypeUsers type, Pageable pageable);
+    Optional<User> findByEmailAndLoans(String email, Loan loan);
 }
